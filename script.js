db1e3b58cd5aeaca46ff604139c6bc00dbaf698d
Eli Orille <eli.orille@yahoo.com>
2022-07-24 13:34:13 -0500

    added play round function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice(){
     let a = Math.floor(Math.random()*3);
-    console.log(a);
     if (a === 2){
         return "rock";
     }
@@ -11,5 +10,31 @@ function getComputerChoice(){
         return "scissors"
     }
 }
-let a = getComputerChoice();
-console.log(a);
+
+function playRound(a,b){
+    if (a === b){
+        return "tie";
+    }
+    if(a === "rock" && b === "paper"){
+        return "you lose " + b + " beats " + a;
+    }
+    if(a==="rock" && b === "scissors"){
+        return "you win " + a + " beats " + b;
+    }
+    if(a === "paper" && b === "scissors"){
+        return "you lose " + b + " beats " +a;
+    }
+    if(a==="paper" && b ==="rock"){
+        return "you win "+a+" beats "+b;
+    }
+    if(a==="scissors"&&b==="rock"){
+        return "you lose "+b+" beats "+a;
+    }
+    if(a==="scissors"&&b==="paper"){
+        return "you win "+a+" beats "+b;
+    }
+}
+const playerSelection = prompt("Rock Paper or Scissors: ");
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerSelection,computerChoice));
