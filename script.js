5ba51c827548f19e7548b3d5b8d377a6aaeca98b
Eli Orille <eli.orille@yahoo.com>
2022-07-27 22:36:50 -0500

    added simple UI

--- script.js
+++ script.js
@@ -10,35 +10,37 @@ function getComputerChoice(){
         return "scissors"
     }
 }
-function game(){
-    let a;
-    let b;
-    let c;
-    let compWins;
-    let playerWins;
-    for (let i =0; i<5; i++){
-        a = prompt("Rock Paper or Scissors: ");
-        b = getComputerChoice();
-        c = playRound(a,b);
-        if (c.includes("win")){
-            playerWins++
-        }
-        if(c.includes("lose")){
-            compWins++
-        }
-        console.log(c);
-    }
-    if(playerWins > compWins){
-        return "You Won";
-    }else if(compWins>playerWins){
-        return "You Lose"
-    }
-    else{
-        return"Tie";
-    }
-}
+// function game(){
+//     let a;
+//     let b;
+//     let c;
+//     let compWins;
+//     let playerWins;
+//     for (let i =0; i<5; i++){
+//         a = prompt("Rock Paper or Scissors: ");
+//         b = getComputerChoice();
+//         c = playRound(a,b);
+//         if (c.includes("win")){
+//             playerWins++
+//         }
+//         if(c.includes("lose")){
+//             compWins++
+//         }
+//         console.log(c);
+//     }
+//     if(playerWins > compWins){
+//         return "You Won";
+//     }else if(compWins>playerWins){
+//         return "You Lose"
+//     }
+//     else{
+//         return"Tie";
+//     }
+// }
 
 function playRound(a,b){
+    // const score = document.querySelector('#score');
+    // score.textContent = `${playerWins} - ${computerWins}`
     if (a === b){
         return "tie";
     }
@@ -61,8 +63,35 @@ function playRound(a,b){
         return "you win "+a+" beats "+b;
     }
 }
-const playerSelection = prompt("Rock Paper or Scissors: ");
-const computerChoice = getComputerChoice();
 
-console.log(playRound(playerSelection,computerChoice));
-console.log(game());
+//console.log(game());
+let playerWins = 0;
+let computerWins = 0;
+const playerSelection = document.querySelectorAll('button');
+playerSelection.forEach((button) => 
+    button.addEventListener('click',() =>{
+        let compChoice = getComputerChoice();
+        let playerChoice = button.id;
+        let a = playRound(playerChoice,compChoice);
+        if(a.includes('win')){
+            playerWins++;
+        }else if(a.includes('lose')){
+            computerWins++;
+        }
+        const results = document.querySelector('#results')
+        results.textContent = a;
+        const score = document.querySelector('#score');
+        score.textContent = `${playerWins} - ${computerWins}`
+        if(playerWins >4 || computerWins>4){
+            playerWins = 0;
+            computerWins = 0;
+            const winner = document.querySelector('#winner');
+            if(playerWins > computerWins){
+                winner.textContent = 'you win';
+            }else{
+                winner.textContent = 'you lose';
+            }
+            return;
+        }
+    })
+);
